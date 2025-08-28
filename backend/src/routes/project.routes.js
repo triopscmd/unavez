4e50487ece185fb1f664e033f0d4de40d2ca8d93
triopscmd <mtrabajoraul@gmail.com>
2025-08-28 17:33:15 +0200

    chore: Bootstrap backend/src/routes/project.routes.js

---
+++ backend/src/routes/project.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const projectController = require('../controllers/project.controller');
+
+const router = express.Router();
+
+router.post('/projects', projectController.createProject);
+router.get('/projects', projectController.getAllProjects);
+router.get('/projects/:id', projectController.getProjectById);
+router.put('/projects/:id', projectController.updateProject);
+router.delete('/projects/:id', projectController.deleteProject);
+
+module.exports = router;
